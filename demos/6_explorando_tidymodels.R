86b69e75902a09f2b2f2b9fe1a24a3605e78ceba
Thiago Silva <thiago.sf.silva@stir.ac.uk>
2025-12-16 08:09:27 -0300

    aula 4 - parte 2

--- demos/6_explorando_tidymodels.R
+++ demos/6_explorando_tidymodels.R
@@ -24,45 +24,8 @@ library(ggplot2) # para a funcao ggplot
 # Plot só da variável alvo
 ggplot(hotels,aes(children)) + geom_bar()
 
-### Passo 3 - organizacao dos dados e 'engenharia de feicoes"
 
-# Para isso usamos o pacote recipes
-library(recipes) # para preparar os dados
-library(dplyr) # para poder usar o pipe %>%
-
-# Criamos uma receita com diversas transformaçoes
-# O pacote recipes oferece alternativas a funcoes do dplyr, lubridate etc
-# com funcoes mais especificas para machine learning
-
-# Primeiro criamos uma lista de feriados para a função step_holiday()
-holiday_list <- c("AllSouls","AshWednesday","ChristmasEve",
-                  "Easter","ChristmasDay","GoodFriday",
-                  "NewYearsDay","PalmSunday")
-
-# A formula define de onde vem os dados e quem sao o alvo e as feicoes
-rf_recipe <- recipe(children ~ ., data = hotels) %>%
-    # converte todas as colunas chr para fct
-    step_string2factor(all_nominal_predictors()) %>%
-    # separa a data em dia, mes e ano
-    step_date(arrival_date) %>%
-    # cria uma varivel binaria feriado/nao-feriado
-    step_holiday(arrival_date, holidays = holiday_list) %>%
-    # remove a coluna original de datas
-    step_rm(arrival_date) %>%
-    # cria variaveis 'dummy' para todas os fatores
-    step_dummy(all_nominal_predictors())
-    # remove variaveis com variancia zero
-    step_zv() %>%
-    # nornaliza todos os preditores numericos
-    step_normalize(all_numeric_predictors())
-
-# Para que a receita seja aplicada, ela precisa ser 'preparada'
-rf_prep <- prep(rf_recipe)
-
-# Receita "Preparada"
-rf_prep
-
-### Passo 4 - Criando a separacao de treino e teste
+### Passo 2 - Criando a separacao de treino e teste
 library(rsample) # Para gerar splits
 
 # Cria uma separacao dos dados em 0.75/0.25,
@@ -75,30 +38,116 @@ data_split <- initial_split(hotels,
 hotel_train <- training(data_split) # Cria o dataset de treino
 hotel_test <- testing(data_split) # Cria o dataset de teste
 
-# Para aplicar a receita preparada, tenmos que 'cozinhar' o dataset
-hotel_train_prep <- bake(rf_prep, new_data = hotel_train)
-hotel_test_prep <- bake(rf_prep, new_data = hotel_test)
 
-# Resultado:
-head(hotel_train_prep)
+### Passo 3 - organizacao dos dados e 'engenharia de feicoes'
 
-# Escolhendo o algoritmo
-rf_mod <- rand_forest(mtry = tune(), trees = tune()) %>%
-    set_engine('ranger') %>%
-    set_mode('classification')
+# Para isso usamos o pacote recipes
+library(recipes) # para preparar os dados
+library(dplyr) # para poder usar o pipe %>%
 
-# Criando o grid para otimização
-rf_grid <- expand.grid(mtry = c(3,5,7),
-                       trees = c(500,1000,5000))
+# Criamos uma receita com diversas transformaçoes
+# O pacote recipes oferece alternativas a funcoes do dplyr, lubridate etc
+# com funcoes mais especificas para machine learning
 
+# Primeiro criamos uma lista de feriados para a função step_holiday()
+holiday_list <- c('AllSouls','AshWednesday','ChristmasEve',
+                  'Easter','ChristmasDay','GoodFriday',
+                  'NewYearsDay','PalmSunday')
 
-# Validacao Cruzada
+# A formula define de onde vem os dados e quem sao o alvo e as feicoes
+rf_recipe <- recipe(children ~ ., data = hotel_train) %>%
+    # converte todas as colunas chr para fct
+    step_string2factor(all_nominal_predictors()) %>%
+    # separa a data em dia, mes e ano
+    step_date(arrival_date) %>%
+    # cria uma varivel binaria feriado/nao-feriado
+    step_holiday(arrival_date, holidays = holiday_list) %>%
+    # remove a coluna original de datas e outras desnecessarias
+    step_rm(arrival_date, hotel, country) %>%
+    # cria variaveis 'dummy' para todas os fatores
+    step_zv() %>%
+    # nornaliza todos os preditores numericos
+    step_normalize(all_numeric_predictors())
 
-# Juntando tudo em um workflow
-rf_workflow <- workflow() %>%
+### Passo 4 - Escolhendo e preparando o modelo
+
+# Escolhendo o algoritmo: Random Forest
+library(parsnip) # Contém os algoritmos de ML
+
+rf_mod <- rand_forest(mtry = tune(), trees = tune()) %>% # quais parametros otimizar?
+    set_engine('ranger', importance = 'impurity') %>% # Qual pacote usar para implementacao do RF?
+    set_mode('classification') # Classificacao ou Regressao?
+
+# Preparando um grid de valores de hiperparametros para otimizacao
+# expand.grid já cria todas as combinações pra gente
+rf_grid <- expand.grid(mtry = c(3,5),
+                       trees = c(50,100))
+
+# Criando 5 folds para validação cruzada
+set.seed(42)
+hotel_folds <- vfold_cv(hotel_train, v = 5, strata = children)
+
+## Passo 6 - executando a otimização!
+library(tune) # para a otimizacao
+library(yardstick) # para a metrica de erro
+library(workflows) # para ligar todos os passos
+
+# Primeiro criamos um 'workflow' conectando os passos anteriores
+rf_workflow <-
+    workflow() %>%
     add_model(rf_mod) %>%
-    add_recipe(rf_recipe) %>%
-    tune_grid(hotel_train,
-              grid = rf_grid,
-              control = control_grid(save_pred = TRUE),
-              metrics = metric_set(roc_auc))
+    add_recipe(rf_recipe)
+
+# Depois 'plugamos' a otimização no workflow para gerar o resultado
+
+rf_tune <- rf_workflow %>%
+    tune_grid(resamples = hotel_folds, # os folds da validação
+              grid = rf_grid, # o grid de parametros que criamos
+              control = control_grid(save_pred = TRUE, # salva os resultados da otimizacao
+                                     verbose = TRUE), # Indica o andamento da otimizacao
+              metrics = metric_set(roc_auc)  # define a métrica
+              )
+
+# Inspecionando o objeto resultante
+rf_tune
+
+# Quais os melhores resultados?
+rf_res <-  rf_tune %>% show_best(metric = 'roc_auc')
+rf_res
+
+# Selecionando o melhor de todos
+rf_best <- rf_tune %>% select_best(metric = 'roc_auc')
+
+# Plotando a ROC
+rf_auc <-
+    rf_tune %>%
+    collect_predictions(parameters = rf_best) %>%
+    roc_curve(children, .pred_children) %>%
+    mutate(model = 'Random Forest')
+
+bind_rows(rf_auc) %>%
+    ggplot(aes(x = 1 - specificity, y = sensitivity, col = model)) +
+    geom_path(lwd = 1.5, alpha = 0.8) +
+    geom_abline(lty = 3) +
+    coord_equal() +
+    scale_color_viridis_d(option = 'plasma', end = .6)
+
+### Passo 7 - ajuste do modelo final e teste
+
+#  Definindo o modelo final
+rf_final <- rf_mod %>% finalize_model(rf_best)
+
+# Atualizando o workflow
+rf_workflow_final <- rf_workflow %>%
+    update_model(rf_final)
+
+# Gerando o ajuste final
+set.seed(345)
+rf_final_fit <- rf_workflow_final %>% last_fit(data_split)
+
+# Gerando varias metricas sobre o modelo final
+rf_final_fit %>% collect_metrics()
+
+### Bonus - Importancia das variaveis
+library(vip)
+rf_final_fit %>% extract_fit_parsnip() %>% vip(num_features = 20)
